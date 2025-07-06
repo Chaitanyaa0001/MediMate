6dd8c64d21705826c097f05196d3fb47eb61499b
chaitanya <chaitanyakhurana0@gmail.com>
2025-07-07 03:27:43 +0530

    chanegs route and folder

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -10,10 +10,6 @@ router.put('/settings/update',checkauth,updateprofile);
 router.put('/settings/password',checkauth,updatepassowrd);
 
 router.put('/settings/photo', checkauth, parser.single('profilephoto'), (req, res, next) => {
-  console.log("Hit upload route");
-  console.log("REQ FILE:", req.file);
-  console.log("REQ USER:", req.user);
-  next();
 }, updateprofilephoto);
 router.delete('/settings/delete',checkauth,deleteuser);
 
